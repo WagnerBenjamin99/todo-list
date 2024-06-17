492625edc89f303198c57d7d24fe18249713ff73
WagnerBenjamin <benjaminwagner@outlook.com>
2024-06-17 19:06:23 -0300

    -

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import './App.css'
 import './TaskComponent.tsx'
-
+import 'react-datepicker/dist/react-datepicker.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
